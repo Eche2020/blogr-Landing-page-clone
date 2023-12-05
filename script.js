74d2059f2f8846acac05229f816e9db1c1803c51
Eche <emmanueleche321@gmail.com>
2023-12-05 21:19:51 +0100

    'commit'

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('mobilemenu')
+const mobileNav = document.getElementById('mobilenav')
+
+mobileNav.style.left = "-100%";
+
+btn.onclick = function(){
+    if (mobileNav.style.left === "-100%") {
+        mobileNav.style.left = "50%";
+        btn.src = "images/icon-close.svg";
+    } else {
+        mobileNav.style.left = "-100%";
+        btn.src = "images/icon.hamburger.svg";
+    }
+}
